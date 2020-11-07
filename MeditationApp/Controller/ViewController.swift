2757646f3323b51d027b3cd94b93a424cb00df95
indiamela <taishi.kusunose@gmail.com>
2020-11-07 09:28:32 +0900

    firstcommit

---
+++ MeditationApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeditationApp
+//
+//  Created by 楠瀬大志 on 2020/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MeditationApp/Controller/breathingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  breathingViewController.swift
+//  MeditationApp
+//
+//  Created by 楠瀬大志 on 2020/11/06.
+//
+
+import UIKit
+
+class breathingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
